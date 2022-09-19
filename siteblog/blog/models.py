6a02ead35caa1f49ada105eea21fcf7e4a41c18c
Diana Uzun <dianauzun2006@gmail.com>
2022-09-19 09:14:30 +0300

    Add a few models to db

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['title']
+
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Тег')
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория', related_name='posts')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'записи'
+        ordering = ['-created_at']
