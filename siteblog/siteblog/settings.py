b446300f210107626bd1c628a3cffeceb1d32433
Diana Uzun <dianauzun2006@gmail.com>
2022-09-22 13:24:40 +0300

    add cache-feature to menu

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -204,3 +204,11 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+#CACHE
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+    }
+}
