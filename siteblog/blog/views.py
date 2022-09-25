70e577cbe600a2155ae522a33a982e8e68fbcb5d
Diana Uzun <dianauzun2006@gmail.com>
2022-09-25 23:51:36 +0300

    add paginator feature

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 
 def index(request):
