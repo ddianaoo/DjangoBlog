e940ee5ad5dec175f1203f36261472f293ea2f6f
Diana Uzun <dianauzun2006@gmail.com>
2022-10-02 18:16:04 +0300

    create feature: show list for each category

--- siteblog/blog/templatetags/menu.py
+++ siteblog/blog/templatetags/menu.py
@@ -1,10 +1,14 @@
 from django import template
 from blog.models import Category
+from django.db.models import *
+
+
 
 register = template.Library()
 
 
 @register.inclusion_tag('blog/menu_cat.html')
 def show_menu(menu_class='menu'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('posts')).filter(cnt__gt=0)
     return {"categories": categories, "menu_class": menu_class}
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -5,16 +5,11 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator
 
 
-def index(request):
-    return render(request, 'blog/index.html')
-
-
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
 
 def get_post(request, slug):
     return HttpResponse('hello')
     #need to change this function
+
 
 class Home(ListView):
     model = Post
@@ -26,3 +21,19 @@ class Home(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'CLASSIC BLOG DESIGN'.title()
         return context
+
+
+class ListPosts(ListView):
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/category.html'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
