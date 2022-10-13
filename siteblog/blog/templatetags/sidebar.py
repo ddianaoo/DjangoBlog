6ac40e4ec80276272f053fa70b587fa97b747acd
Diana Uzun <dianauzun2006@gmail.com>
2022-10-13 15:44:54 +0300

    show tags in sidebar

--- siteblog/blog/templatetags/sidebar.py
+++ siteblog/blog/templatetags/sidebar.py
@@ -10,3 +10,9 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by("-views")[:cnt]
     return {"posts": posts}
+
+
+@register.inclusion_tag('blog/list_of_tags.html')
+def get_tags():
+    tags = Tag.objects.annotate(cnt=Count('posts')).filter(cnt__gt=0)
+    return {"tags": tags}
